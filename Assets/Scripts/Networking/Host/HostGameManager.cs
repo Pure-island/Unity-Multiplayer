921f0a6c68140053f95e10642bfca8646e424792
Dapper Dino <natemi1010@gmail.com>
2023-04-21 11:31:03 +0100

    13 Connection Approval

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
@@ -17,6 +18,8 @@ public class HostGameManager
     private Allocation allocation;
     private string joinCode;
     private string lobbyId;
+
+    private NetworkServer networkServer;
 
     private const int MaxConnections = 20;
     private const string GameSceneName = "Game";
@@ -62,18 +65,30 @@ public class HostGameManager
                     )
                 }
             };
+            string playerName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Unknown");
             Lobby lobby = await Lobbies.Instance.CreateLobbyAsync(
-                "My Lobby", MaxConnections, lobbyOptions);
-            
+                $"{playerName}'s Lobby", MaxConnections, lobbyOptions);
+
             lobbyId = lobby.Id;
 
             HostSingleton.Instance.StartCoroutine(HearbeatLobby(15));
         }
-        catch(LobbyServiceException e)
+        catch (LobbyServiceException e)
         {
             Debug.Log(e);
             return;
         }
+
+        networkServer = new NetworkServer(NetworkManager.Singleton);
+
+        UserData userData = new UserData
+        {
+            userName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Missing Name")
+        };
+        string payload = JsonUtility.ToJson(userData);
+        byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = payloadBytes;
 
         NetworkManager.Singleton.StartHost();
 
@@ -83,7 +98,7 @@ public class HostGameManager
     private IEnumerator HearbeatLobby(float waitTimeSeconds)
     {
         WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
-        while(true)
+        while (true)
         {
             Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
             yield return delay;
